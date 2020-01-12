796c34edaffc12861e464c131ed29674df4a57de
jwp0116 <cong0116@naver.com>
2020-01-12 14:00:16 +0900

    changed logic to calculate at server (+ lodash)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url')
 const port = process.env.PORT || 55810;
+var _ = require('lodash');
 
 let server = http.createServer();
 
@@ -10,36 +11,94 @@ function uuid () {
   return Math.random().toString(36).substr(2, 12).toUpperCase();
 };
 
+class Project {
+  constructor(id) {
+    this.id = id;
+    this.clients = [];
+    this.vc = [];
+  }
+  join(uuid) {
+    this.clients.push(uuid);
+  }
+  unjoin(uuid) {
+    _.remove(this.clients, (e) => e == uuid);
+  }
+  broadcast(msg) {    
+    let sendTarget = _.filter([...wss.clients], ({ uuid }) => _.includes(this.clients, uuid));
+    const sendMsg = JSON.stringify(msg);
+    _.forEach(sendTarget, (client) => {
+      console.log(client.uuid)
+      client.send(sendMsg)
+    })
+    console.log(`${this.id}이 [ ${_.map(sendTarget, (e) => e.uuid).join(', ')} ]에게 데이터를 보냄: ${sendMsg}`)
+  }
+  initVariable(id, val) {
+    return this.vc.push({ id: id, value: val }) -1
+  }
+  getVariable(id) {
+    return _.find(this.vc, { id: id }) || this.vc[this.initVariable(id, 0)];
+  }
+  getList(id) {
+    return _.find(this.vc, { id: id }) || this.vc[this.initVariable(id, [])];
+  }
+}
+function isNumber(value) {
+  if ("number" == typeof value) return true;
+  return !("string" != typeof value || !/^-?\d+\.?\d*$/.test(value))
+}
+
+let projects = {};
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws, req) {
   ws.uuid = uuid();
   ws.projectID = url.parse(req.url, true).query.id;
+  if (!projects.hasOwnProperty(ws.projectID)) {
+    projects[ws.projectID] = new Project(ws.projectID);
+  }
+  projects[ws.projectID].join(ws.uuid);
+
   console.log(`${ws.uuid}가 ${ws.projectID}에 연결함!`);
   
   ws.on('message', function incoming(data) {
-    switch (JSON.parse(data).type) {
-      case 'syncPls':
-        const syncTarget = wss.clients.find(client => client.readyState === WebSocket.OPEN && client.projectID === ws.projectID)
-        ws.syncPls = true;
-        syncTarget.send(JSON.stringify({ type: "syncPls" }));
+    const message = JSON.parse(data);
+    console.log(`${ws.uuid}가 ${ws.projectID}로 보낸 데이터를 받음: ${data}`);
+
+    let variable;
+    switch (message.type) {
+      case 'setVariable':
+        variable = projects[ws.projectID].getVariable(message.target.id);
+        variable.value = message.value;
+        projects[ws.projectID].broadcast({type: 'variable', target: message.target, value: variable.value});
         break;
-      case 'sync':
-        const syncPls = wss.clients.find(client => client.readyState === WebSocket.OPEN && client.projectID === ws.projectID && syncPls)
-        delete ws.syncPls;
-        syncPls.send(JSON.stringify({ type: "sync", data: data }));
+      case 'changeVariable':
+        variable = projects[ws.projectID].getVariable(message.target.id);
+        let sumValue;
+        if(isNumber(variable.value) && isNumber(message.value))
+          sumValue = Number(variable.value) + Number(message.value)
+        else
+          sumValue = `${variable.value}${message.value}`
+        variable.value = sumValue;
+        projects[ws.projectID].broadcast({ type: 'variable', target: message.target, value: variable.value });
+        break;
+      case 'pushList': 
+        variable = projects[ws.projectID].getList(message.target.id);
+        variable.value.push({ data: message.value })
+        projects[ws.projectID].broadcast({ type: 'list', target: message.target, value: variable.value });
+        break;
+      case 'removeList':
+        variable = projects[ws.projectID].getList(message.target.id);
+        variable.value.splice(message.value - 1, 1);
+        projects[ws.projectID].broadcast({ type: 'list', target: message.target, value: variable.value });
         break;
       default:
-        wss.clients.forEach(function each(client) {
-          if (client.readyState === WebSocket.OPEN && client.projectID === ws.projectID && client !== ws) client.send(data);
-        });
-        console.log(`${ws.uuid}가 ${ws.projectID}로 데이터를 보냄: ${data}`);
         break;
-    }
-
+      }
   });
 
   ws.on('close', function close() {
+    projects[ws.projectID].unjoin(ws.uuid);
     console.log(`${ws.uuid}의 연결이 끊김!`)
   });
 });
